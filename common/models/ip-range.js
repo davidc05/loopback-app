eab07e85d874258e435cdfa2798725ba9a690f1c
David Coelho <davidcoelho058@gmail.com>
2018-11-07 22:53:44 -0500

    Added new model ipRange

---
+++ common/models/ip-range.js
@@ -0,0 +1,28 @@
+'use strict';
+var request = require('request');
+
+module.exports = function(IpRange) {
+    IpRange.getIpDetailRangesByNetworkName = function(networkName, cb) {
+        request('https://api.musubu.io/MusubuAPI/Musubu?NetworkName='+networkName+'&key=b9c4896dd776e2e61a937a01aa3d1ac8&format=JSON&level=verbose', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                cb(null, JSON.parse(body));
+            }
+        });
+    };
+    IpRange.remoteMethod(
+        'getIpDetailRangesByNetworkName', {
+            accepts: {
+                arg: 'networkName',
+                type:'string'
+            },
+            http: {
+                path: '/getIpDetailRangesByNetworkName',
+                verb: 'get'
+            },
+            returns: {
+                arg: 'ipRanges',
+                type: 'array'
+            }
+        }
+    );
+};
