92a742dd7fa84beb54fb07ddd066a2d96162a835
David Coelho <davidcoelho058@gmail.com>
2018-11-15 00:12:27 -0500

    Added call for Network Type dat

--- common/models/ip-range.js
+++ common/models/ip-range.js
@@ -25,4 +25,28 @@ module.exports = function(IpRange) {
             }
         }
     );
+
+    IpRange.getIpDetailRangesByNetworkType = function(networkType, cb) {
+        request('https://api.musubu.io/MusubuAPI/Musubu?NetworkType='+networkName+'&key=b9c4896dd776e2e61a937a01aa3d1ac8&format=JSON&level=verbose', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                cb(null, JSON.parse(body));
+            }
+        });
+    };
+    IpRange.remoteMethod(
+        'getIpDetailRangesByNetworkType', {
+            accepts: {
+                arg: 'networkType',
+                type:'string'
+            },
+            http: {
+                path: '/getIpDetailRangesByNetworkType',
+                verb: 'get'
+            },
+            returns: {
+                arg: 'ipRanges',
+                type: 'array'
+            }
+        }
+    );
 };
