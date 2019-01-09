a2a245a30cf945ee8034f81f457ccb04512b83d5
David Coelho <davidcoelho058@gmail.com>
2019-01-09 15:52:17 -0600

    create watchlist service to provide daily data from mongodb

--- common/models/saved-search.js
+++ common/models/saved-search.js
@@ -1,5 +1,31 @@
 'use strict';
+const watchlistService = require('../services/watchlistService');
 
 module.exports = function(Savedsearch) {
+  Savedsearch.savedSearches = (userEmail, createdDate, cb) => {
+    watchlistService.getWatchlistByDate(userEmail, createdDate, cb);
+  };
 
+  Savedsearch.remoteMethod(
+    'savedSearches', {
+      accepts: [
+        {
+          arg: 'userEmail',
+          type: 'string',
+        },
+        {
+          arg: 'createdDate',
+          type: 'string',
+        },
+      ],
+      http: {
+        path: '/savedSearches',
+        verb: 'get',
+      },
+      returns: {
+        arg: 'searches',
+        type: 'object',
+      },
+    }
+  );
 };
