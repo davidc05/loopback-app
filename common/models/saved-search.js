5e93ca406522eed1ed6499a05757d886500de4f0
David Coelho <davidcoelho058@gmail.com>
2019-01-16 11:50:20 -0600

    remove unnecessary mongo connection

--- common/models/saved-search.js
+++ common/models/saved-search.js
@@ -1,9 +1,42 @@
 'use strict';
-const watchlistService = require('../services/watchlistService');
+const moment = require('moment');
 
 module.exports = function(Savedsearch) {
-  Savedsearch.savedSearches = (userEmail, createdDate, cb) => {
-    watchlistService.getWatchlistByDate(userEmail, createdDate, cb);
+
+  Savedsearch.savedSearches = function(userEmail, createdDate, cb) {
+
+        const year = moment(createdDate).format('YYYY');
+        const month = moment(createdDate).format('MM');
+        const day = moment(createdDate).format('DD');
+
+        const currentTimeZone = new Date().getTimezoneOffset() / 60;
+
+        const startDate = moment(new Date(year, month - 1, day, 0, 0, 0).setHours(
+            new Date(year, month - 1, day, 0, 0, 0).getHours() - currentTimeZone
+        )).toISOString();
+
+        const endDate = moment(
+            new Date(year, month - 1, day, 0, 0, 0).setHours(23 - currentTimeZone, 59, 59)
+        ).toISOString();
+
+        Savedsearch.find({
+            where: {
+                and: [
+                    {
+                        userEmail: userEmail,
+                    },
+                    {
+                        createdOn: {lte: new Date(endDate)},
+                    },
+                    {
+                        createdOn: {gte: new Date(startDate)},
+                    },
+                ],
+            },
+        }, function(err, items) {
+            if (err) throw err;
+            cb(null, items);
+        });
   };
 
   Savedsearch.remoteMethod(
