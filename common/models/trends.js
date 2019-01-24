98ff20d75eb24fd36b4df9d045531d9e3ad33680
David Coelho <davidcoelho058@gmail.com>
2019-01-24 08:19:34 -0600

    create Trends model

---
+++ common/models/trends.js
@@ -0,0 +1,48 @@
+'use strict';
+const { BigQuery } = require('@google-cloud/bigquery');
+
+const bigquery = new BigQuery({
+    projectId: 'carbon-aquifer-220218',
+});
+
+module.exports = function(Trends) {
+    Trends.watchlistByThreatlevel = function(userEmail, cb) {
+        // Watchlist by Threat level
+        const sqlQuery = `SELECT queryName, AVG(ips.threat_potential_score_pct) as avg_threat_score
+            FROM musubu_watchlist.savedSearch, 
+            UNNEST(ips) as ips
+            WHERE userEmail = "${userEmail}"
+            GROUP BY queryName
+            ORDER BY avg_threat_score DESC`;
+
+        const options = {
+            query: sqlQuery,
+            location: 'US',
+        };
+
+        bigquery.query(options)
+            .then(res => {
+                cb(null, res[0]);
+            })
+            .catch(err => {
+                cb(null, err);
+            });
+    };
+
+    Trends.remoteMethod(
+        'watchlistByThreatlevel', {
+            accepts: {
+                arg: 'userEmail',
+                type: 'string',
+            },
+            http: {
+                path: '/watchlistByThreatlevel',
+                verb: 'get',
+            },
+            returns: {
+                arg: 'trends',
+                type: 'object',
+            },
+        }
+    );
+};
