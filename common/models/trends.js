ccffa123305864de568c326d6c5539d9311edb50
David Coelho <davidcoelho058@gmail.com>
2019-02-06 11:54:57 -0600

    add blacklistClass to IP HotList from BigQuery

--- common/models/trends.js
+++ common/models/trends.js
@@ -1,14 +1,14 @@
 'use strict';
-const { BigQuery } = require('@google-cloud/bigquery');
+const {BigQuery} = require('@google-cloud/bigquery');
 
 const bigquery = new BigQuery({
-    projectId: 'carbon-aquifer-220218',
+  projectId: 'carbon-aquifer-220218',
 });
 
 module.exports = function(Trends) {
-    Trends.getTrends = function(userEmail, cb) {
+  Trends.getTrends = function(userEmail, cb) {
         // Watchlist by Threat level
-        const watchlistbyThreatlevel = `SELECT _id as id, queryName, AVG(ips.threat_potential_score_pct) as avg_threat_score
+    const watchlistbyThreatlevel = `SELECT _id as id, queryName, AVG(ips.threat_potential_score_pct) as avg_threat_score
             FROM musubu_watchlist.savedSearch, 
             UNNEST(ips) as ips
             WHERE userEmail = "${userEmail}"
@@ -16,7 +16,7 @@ module.exports = function(Trends) {
             ORDER BY avg_threat_score DESC`;
 
         //  Top 10 IPs by threat score - last 90 days
-        const top10IPsbyThreat = `SELECT ips.ipaddress, ips.threat_potential_score_pct, ips.blacklist_class
+    const top10IPsbyThreat = `SELECT ips.ipaddress, ips.threat_potential_score_pct, ips.blacklist_class
             FROM musubu_watchlist.savedSearch, 
             UNNEST(ips) as ips
             WHERE userEmail = "${userEmail}" and createdOn >= TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL -90 DAY)
@@ -25,7 +25,7 @@ module.exports = function(Trends) {
             LIMIT 10`;
 
         // Threat Type Breakdown - Last 90 Days
-        const threatTypeBreakdown = `
+    const threatTypeBreakdown = `
           WITH countsByClass AS (
             SELECT 
                 ss.queryname as queryName,
@@ -66,7 +66,7 @@ module.exports = function(Trends) {
         `;
 
         // threat score volatility
-        const threatScoreVolatility = `
+    const threatScoreVolatility = `
           WITH rawData AS (
             SELECT
                 DISTINCT ss.queryname,
@@ -104,7 +104,7 @@ module.exports = function(Trends) {
         `;
 
         // Watchlist trends - last 90 days
-        const watchlistTrends = `
+    const watchlistTrends = `
           SELECT AVG(ips.threat_potential_score_pct) as avg, queryName, createdOn
             FROM musubu_watchlist.savedSearch,
               UNNEST(ips) as ips
@@ -113,14 +113,15 @@ module.exports = function(Trends) {
         `;
 
         // IP hot list
-        const ipHotList = `
+    const ipHotList = `
           WITH query1 AS (
             SELECT
                 ips.ipaddress as ipAddress,
                 IFNULL(bc.severity_rating, 0) as severityRating,
                 IFNULL(ips.blacklist_network_neighbor_cnt, 0) as neighborCount,
                 IFNULL(ips.blacklist_observations, 0) as blacklistCount,
-                ips.threat_potential_score_pct as threatScore
+                ips.threat_potential_score_pct as threatScore,
+                ips.blacklist_class as blacklistClass
             FROM
               musubu_watchlist.savedSearch ss,
               musubu_watchlist.blacklistClasses bc,
@@ -132,51 +133,52 @@ module.exports = function(Trends) {
 
             SELECT
               query1.ipAddress as ipAddress,
-              (severityRating + neighborCount + blacklistCount + threatScore) as severityScore
+              (severityRating + neighborCount + blacklistCount + threatScore) as severityScore,
+              query1.blacklistClass as blacklistClass
             FROM query1
-            GROUP BY ipAddress, severityScore
+            GROUP BY ipAddress, severityScore, blacklistClass
             ORDER BY severityScore desc;
         `;
 
-        const promistList = [
-            bigquery.query({query: watchlistbyThreatlevel, location: 'US'}).catch(err => err),
-            bigquery.query({query: top10IPsbyThreat, location: 'US'}).catch(err => err),
-            bigquery.query({query: threatTypeBreakdown, location: 'US'}).catch(err => err),
-            bigquery.query({query: threatScoreVolatility, location: 'US'}).catch(err => err),
-            bigquery.query({query: watchlistTrends, location: 'US'}).catch(err => err),
-            bigquery.query({query: ipHotList, location: 'US'}).catch(err => err),
-        ];
+    const promistList = [
+      bigquery.query({query: watchlistbyThreatlevel, location: 'US'}).catch(err => err),
+      bigquery.query({query: top10IPsbyThreat, location: 'US'}).catch(err => err),
+      bigquery.query({query: threatTypeBreakdown, location: 'US'}).catch(err => err),
+      bigquery.query({query: threatScoreVolatility, location: 'US'}).catch(err => err),
+      bigquery.query({query: watchlistTrends, location: 'US'}).catch(err => err),
+      bigquery.query({query: ipHotList, location: 'US'}).catch(err => err),
+    ];
 
-        Promise.all(promistList)
+    Promise.all(promistList)
             .then(res => {
-                cb(null, {
-                    watchlistbyThreatlevel: res[0][0],
-                    top10IPsbyThreat: res[1][0],
-                    threatTypeBreakdown: res[2][0],
-                    threatScoreVolatility: res[3][0],
-                    watchlistTrends: res[4][0],
-                    ipHotList: res[5][0],
-                });
+              cb(null, {
+                watchlistbyThreatlevel: res[0][0],
+                top10IPsbyThreat: res[1][0],
+                threatTypeBreakdown: res[2][0],
+                threatScoreVolatility: res[3][0],
+                watchlistTrends: res[4][0],
+                ipHotList: res[5][0],
+              });
             })
             .catch(err => {
-                cb(null, err);
+              cb(null, err);
             });
-    };
+  };
 
-    Trends.remoteMethod(
+  Trends.remoteMethod(
         'getTrends', {
-            accepts: {
-                arg: 'userEmail',
-                type: 'string',
-            },
-            http: {
-                path: '/getTrends',
-                verb: 'get',
-            },
-            returns: {
-                arg: 'trends',
-                type: 'object',
-            },
+          accepts: {
+            arg: 'userEmail',
+            type: 'string',
+          },
+          http: {
+            path: '/getTrends',
+            verb: 'get',
+          },
+          returns: {
+            arg: 'trends',
+            type: 'object',
+          },
         }
     );
 };
